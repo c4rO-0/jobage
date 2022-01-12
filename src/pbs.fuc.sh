44d1c613e1808ce3326073d336f98fa487993db4
Sheng Bi <ruc.bs.plu@hotmail.com>
2022-01-12 16:11:20 +0800

    Update pbs queue

--- src/pbs.fuc.sh
+++ src/pbs.fuc.sh
@@ -37,43 +37,11 @@ else
 fi
 
 _jobage_lsf_raw_xml_dinfo="$_jobage_wPath/qstat.raw.xml"
-_jobage_lsf_xml_dinfo="$_jobage_wPath/qstat.xml"
 _jobage_lsf_json_dinfo="$_jobage_wPath/qstat.json"
 
 
 # basic func
 source "$_jobage_pbs_fuc_srcPath/core.fuc.sh"
-
-function _jobage_pbs_raw() {
-    # help
-    if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-        echo '|-get job raw info'
-        return
-    fi
-
-    _jobage_lsf_info=$(bjobs -u $USER -o "jobid:7 queue:10 job_name:20 stat:5 run_time exec_host:12 sub_cwd")
-
-    if [[ "$_jobage_lsf_info" == *"No unfinished job found"* ]]; then
-        echo ""
-    else
-        _jobage_lsf_secs=$(echo "$_jobage_lsf_info" | awk '{print $5}' | tail --lines=+2 | sed 's/ //g')
-        
-        _jobage_lsf_info_r=$_jobage_lsf_info
-        OLD_IFS="$IFS"
-        IFS=
-        while read _jobage_lsf_t
-        do
-                _jobage_lsf_tRead_raw=$(_jobage_displaytime "$_jobage_lsf_t")
-                _jobage_lsf_tRead=$(printf '%-16s' "$_jobage_lsf_tRead_raw")
-                _jobage_lsf_info_r=$(echo "$_jobage_lsf_info_r" | sed "s/$_jobage_lsf_t second(s)/$_jobage_lsf_tRead/g")
-        done <<< "$_jobage_lsf_secs"
-        IFS="$OLD_IFS"
-        
-        _jobage_lsf_info_r=$(echo "$_jobage_lsf_info_r" | sed "s|\$HOME|$HOME|g")
-        echo "$_jobage_lsf_info_r"
-    fi
-}
-
 
 function _jobage_pbs_save_queue() {
 
@@ -89,40 +57,38 @@ function _jobage_pbs_save_queue() {
 
     # read json
     # https://github.com/owenthereal/jqplay
-    "$_jobage_pbs_fuc_srcPath/jq-linux64" ".Data.Job[] | [.Job_Id, .Job_Name, .job_state, .init_work_dir, .resources_used.walltime]" "$_jobage_lsf_json_dinfo" | sed -e ':a;N;$!ba;s/,\n//g; s/\[//g; s/\]//g; s/"//g;'
+    _jobage_pbs_jobs=$("$_jobage_pbs_fuc_srcPath/jq-linux64" ".Data.Job[] | [.Job_Id, .queue, .Job_Name, .job_state, .resources_used.walltime, .Resource_List.nodes, .init_work_dir]" "$_jobage_lsf_json_dinfo" | sed -E -e ':a;N;$!ba;s/(,|\[|\])\n//g; s/"//g; s/\[//g; s/\]//g;' | sed 's/:ppn=/x/g; s/null/0/g' |tr -s "\n" | sort -k 2n)
     
-    # _jobage_lsf_timestampNow=$(date +%s)
+    _jobage_pbs_timestampNow=$(date +%s)
 
-    # if [ -f "$_jobage_dinfo1" ] && [ -s "$_jobage_dinfo1" ]; then
-    #     if [ -f "$_jobage_dinfo2" ] && [ -s "$_jobage_dinfo2" ]; then
-    #         _jobage_lsf_timestampPre=$(head -n1 "$_jobage_dinfo2" | awk '{print $7}')
-    #         # echo $timestampNow $timestampPre 
-    #         # echo $(echo "scale=0 ;$timestampNow-$timestampPre > 300" | bc )
-    #         if [[ $(echo "scale=0 ;$_jobage_lsf_timestampNow-$_jobage_lsf_timestampPre > 300" | bc ) -eq 1 ]]; then
-    #         cp "$_jobage_dinfo1" "$_jobage_dinfo2"
-    #         fi
-    #         # echo 'cp end'
-    #     else
-    #         cp "$_jobage_dinfo1" "$_jobage_dinfo2"
-    #     fi
-    # fi
+    if [ -f "$_jobage_dinfo1" ] && [ -s "$_jobage_dinfo1" ]; then
+        if [ -f "$_jobage_dinfo2" ] && [ -s "$_jobage_dinfo2" ]; then
+            _jobage_pbs_timestampPre=$(head -n1 "$_jobage_dinfo2" | awk '{print $7}')
+            # echo $timestampNow $timestampPre 
+            # echo $(echo "scale=0 ;$timestampNow-$timestampPre > 300" | bc )
+            if [[ $(echo "scale=0 ;$_jobage_pbs_timestampNow-$_jobage_pbs_timestampPre > 300" | bc ) -eq 1 ]]; then
+            cp "$_jobage_dinfo1" "$_jobage_dinfo2"
+            fi
+            # echo 'cp end'
+        else
+            cp "$_jobage_dinfo1" "$_jobage_dinfo2"
+        fi
+    fi
 
-    # _jobage_lsf_jobs=$(_jobage_lsf_raw | tail -n +2 | sort -k 2n)
-
-    # { { date ; echo " " $_jobage_lsf_timestampNow ;}  | tr -d '\n' ; echo ; } > "$_jobage_dinfo1"
+    { { date ; echo " " $_jobage_pbs_timestampNow ;}  | tr -d '\n' ; echo ; } > "$_jobage_dinfo1"
     
-    # # echo "debug:--->"
-    # # cat ~/.local/sq.dat
-    # # echo "debug---|"
-    # echo "----------" >> "$_jobage_dinfo1"
+    # echo "debug:--->"
+    # cat ~/.local/sq.dat
+    # echo "debug---|"
+    echo "----------" >> "$_jobage_dinfo1"
 
-    # printf "%6s %10s %11s %10s %3s %8s %8s %s\n" "num" "JOBID" "PARTITION" "NAME" "ST" "TIME" "NODES" "WORK_DIR" >>  "$_jobage_dinfo1"
+    printf "%6s %10s %11s %10s %3s %8s %11s %s\n" "num" "JOBID" "PARTITION" "NAME" "ST" "TIME" "NODExCPU" "WORK_DIR" >>  "$_jobage_dinfo1"
     # # echo "$jobs" | nl -v 1
     
-    # _jobage_array_jobDir=($(echo "$_jobage_lsf_jobs" | awk '{print $7}'))
-    # _jobage_array_jobID=($(echo "$_jobage_lsf_jobs" | awk '{print $1}'))
+    _jobage_array_jobDir=($(echo "$_jobage_lsf_jobs" | awk '{print $7}'))
+    _jobage_array_jobID=($(echo "$_jobage_lsf_jobs" | awk '{print $1}'))
 
-    # echo "$_jobage_lsf_jobs" | nl -v 1 | sed "s/\/.*\/$USER/~/g" | sed 's/\/\.\///g' | sed 's/\$HOME/~/g' >> "$_jobage_dinfo1"
+    echo "$_jobage_lsf_jobs" | nl -v 1 | sed "s/\/.*\/$USER/~/g" | sed 's/\/\.\///g' | sed 's/\$HOME/~/g' >> "$_jobage_dinfo1"
 }
 
 
@@ -190,8 +156,7 @@ function _jobage_pbs_save_queue() {
 # public and unique function list
 function _jobage_save_queue()
 {
-    echo "test"
-    # _jobage_pbs_save_queue "$@"
+    _jobage_pbs_save_queue "$@"
 }
 
 # function _jobage_cancel_index()
