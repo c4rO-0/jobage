bfdf4d6c5abb66fa591525ee76fda06ad3a56c86
Sheng Bi <ruc.bs.plu@hotmail.com>
2022-01-15 14:10:28 +0800

    Update version

--- src/version.sh
+++ src/version.sh
@@ -5,6 +5,7 @@ else
     _jobage_version_fuc_srcPath=$(dirname $(readlink -f "$0"))
 fi
 
-_jobage_version=$(git show --no-color --pretty=oneline --abbrev-commit | head -n 1);
+_jobage_version=$(git show --no-color --pretty=oneline --abbrev-commit | head -n 1 | awk '{print $1}');
+_jobage_date=$(date +"%Y-%m-%d")
 
-echo "| jobage version : $_jobage_version" > "$_jobage_version_fuc_srcPath/version.dat"
+echo "| jobage version : $_jobage_version on $_jobage_date"> "$_jobage_version_fuc_srcPath/version.dat"
